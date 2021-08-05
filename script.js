be582717750debe6eb4da33b7ed88811744528a9
Srikar-ai <70573179+Srikar-ai@users.noreply.github.com>
2021-08-05 21:43:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-/**
- * Weather App
- * DONE: Complete getWeatherData() to return json response Promise
- * DONE: Complete searchCity() to get user input and get data using getWeatherData()
- * DONE: Complete showWeatherData() to set the data in the the html file from response
- */
 
-// API_KEY for maps api
 let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
-
-/**
- * Retrieve weather data from openweathermap
- * HINT: Use fetch()
- * HINT: URL should look like this: 
- * https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=a8e71c9932b20c4ceb0aed183e6a83bb&units=imperial
- */
 getWeatherData = (city) => {
   const URL = "https://api.openweathermap.org/data/2.5/weather";
   let U=`${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
@@ -21,17 +7,8 @@ getWeatherData = (city) => {
   return mypromise.then((given)=>{
     return given.json();
   })
-
-  
-  //HINT: Use template literals to create a url with input and an API key
-
-  //CODE GOES HERE
 }
 
-/**
- * Retrieve city input and get the weather data
- * HINT: Use the promise returned from getWeatherData()
- */
 searchCity = () => {
   const city = document.getElementById('city-input').value;
   getWeatherData(city)
@@ -40,18 +17,10 @@ searchCity = () => {
   }).catch((error)=>{
     console.log(error);
   })
-
-
-  // CODE GOES HERE
-
 }
 
-/**
- * Show the weather data in HTML
- * HINT: make sure to console log the weatherData to see how the data looks like
- */
+
 showWeatherData = (weatherData) => {
-  //CODE GOES HERE
   console.log(weatherData);
   let name=document.getElementById("city-name");
   name.innerHTML=weatherData.name;
